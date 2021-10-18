5b18dec33739908fb70db8bf883c34864b60cf6b
LiYan20576 <2673353105@qq.com>
2021-10-18 19:37:19 +0800

    增加了serve.txt

--- router/router.js
+++ router/router.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Pro = require('../models/product')
+const Serve = require('../models/serve')
 router.get('/product', (req, res) => {
     Pro.find()
         .then(function (data) {
@@ -9,7 +10,25 @@ router.get('/product', (req, res) => {
         })
 
 })
-
+router.get('/product/:id', (req, res) => {
+    var id = req.params["id"];
+    Pro.findById(id)
+        .then(function (data1) {
+            var num = data1.class
+            // console.log(num)
+            Pro.where({ class: num })
+                .then(function (data2) {
+                    var a = data2.length >= 3 ? 3 : data2.length
+                    var data = new Array
+                    for (var i = 0; i < a; i++) {
+                        var Rand = Math.floor(Math.random() * (data2.length - i));
+                        data[i] = data2[Rand]
+                        data2.splice(Rand, 1)
+                    }
+                    res.render('pro.html', { pro: data1, about: data })
+                })
+        })
+})
 router.get('/index', (req, res) => {
     res.render('index.html')
 })
@@ -38,25 +57,21 @@ router.get('/equipment', (req, res) => {
     res.render('equipment.html')
 })
 router.get('/fwal', (req, res) => {
-    res.render('fwal.html')
-})
-router.get('/product/:id', (req, res) => {
-    var id = req.params["id"];
-    Pro.findById(id)
-        .then(function (data1) {
-            var num = data1.class
-            // console.log(num)
-            Pro.where({ class: num })
-                .then(function (data2) {
-                    var a = data2.length >= 3 ? 3 : data2.length
-                    var data=new Array
-                    for(var i=0;i<a;i++){
-                        var Rand = Math.floor(Math.random()*(data2.length-i));
-                        data[i]=data2[Rand]
-                         data2.splice(Rand,1)
-                    }
-                    res.render('pro.html', { pro: data1, about: data })
-                })
+    Serve.find({page:'1'})
+        .then(function (data) {
+            console.log(data)
+            res.render('fwal.html', { serve: data ,page:'1'})
         })
+
 })
+router.get('/fwal/:id', (req, res) => {
+    var id = req.params["id"];
+    Serve.find({page:id})
+        .then(function (data) {
+            // console.log(data)
+            res.render('fwal.html', { serve: data ,page:id})
+        })
+
+})
+
 module.exports = router;
